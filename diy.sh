2d5f560d5bbeec037307ac90761f79ce9c69e173
Jun Liu <goofysun@gmail.com>
2020-06-05 01:38:46 +0800

    Add files via upload

---
+++ diy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 本脚本工作目录必须是git仓库的主目录
+
+# Add Some Package
+
+mkdir -p package/custom
+cd package/custom
+# git clone https://github.com/coolsnowwolf/packages
+git clone https://github.com/kenzok8/small
+git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/Lienol/openwrt-package
+#kenzok8 已包含 git clone https://github.com/fw876/helloworld
+#kenzok8 已包含 git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+#kenzok8 已包含 git clone https://github.com/vernesong/OpenClash
+git clone https://github.com/tty228/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome
+#git clone https://github.com/pymumu/luci-app-smartdns
+git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/goofysun/luci-app-baidupcs-web
+git clone https://github.com/lisaac/luci-app-dockerman
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/honwen/luci-app-aliddns
+svn co https://github.com/jsda/packages2/trunk/ntlf9t/luci-app-advancedsetting
+
+
+cd ../../
+# Modify default IP
+
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+
+# Add some default settings
+
+#\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+curl -fsSL https://raw.githubusercontent.com/goofysun/My-action/master/lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
