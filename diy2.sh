a1476657c8d17f5a2bbdc0692ac22b7034558a1d
Jun Liu <goofysun@gmail.com>
2020-06-07 13:17:44 +0800

    替换 argon主题源

--- diy2.sh
+++ diy2.sh
@@ -3,6 +3,7 @@
 # 本脚本工作目录必须是git仓库的主目录
 
 cd package/lean/
+rm -rf luci-theme-argon
 svn co https://github.com/openwrt/luci/trunk/applications/luci-app-acme
 git clone https://github.com/lisaac/luci-app-diskman
 git clone https://github.com/brvphoenix/luci-app-wrtbwmon
@@ -14,8 +15,9 @@ git clone https://github.com/goofysun/luci-app-baidupcs-web
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
 git clone https://github.com/goofysun/openwrt-adguardhome
 git clone https://github.com/honwen/luci-app-aliddns
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-theme-argon
 cd ../../
 
 # Add some default settings
@@ -24,5 +26,3 @@ mv ../zzz-default-settings  package/lean/default-settings/files/zzz-default-sett
 rm -rf package/feeds/kenzo/luci-app-adguardhome
 rm -rf package/feeds/kenzo/adguardhome
 rm -rf package/feeds/kenzo/luci-app-aliddns
-
-
