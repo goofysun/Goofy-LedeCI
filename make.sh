05d0f5d3ad87424e5e5865041a8b46d68cc93747
Jun Liu <goofysun@gmail.com>
2020-06-08 01:49:10 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,14 +1,14 @@
 
 
-sudo swapoff /swapfile
-sudo rm -rf /swapfile /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
+swapoff /swapfile
+rm -rf /swapfile /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
 docker rmi `docker images -q`
-sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
-sudo -E apt-get -qq update
-sudo -E apt-get -qq install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib g++-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler antlr3 gperf
-sudo -E apt-get -qq autoremove --purge
-sudo -E apt-get -qq clean
-sudo df -h
+apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+apt-get -qq update
+apt-get -qq install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib g++-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler antlr3 gperf
+apt-get -qq autoremove --purge
+apt-get -qq clean
+df -h
 curl -fsSL https://github.com/goofysun/Goofy-LedeCI/blob/master/.bashrc >> ~/.bashrc
 sudo timedatectl set-timezone "Asia/Shanghai"   
 if [ -d "lede" ];then
