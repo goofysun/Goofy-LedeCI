eab057b7ae3e5599da72fdec6d7beda748f05980
Jun Liu <goofysun@gmail.com>
2020-06-09 12:30:01 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -11,6 +11,7 @@ sudo timedatectl set-timezone "Asia/Shanghai"
 if [ -d "lede" ];then
   cd lede
   git reset --hard
+  git checkout master
   git pull
 else
   git clone  https://github.com/coolsnowwolf/lede
