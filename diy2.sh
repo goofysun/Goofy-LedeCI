99da10a21242c5db4bb6e16a4e87a6b545b1aac2
Jun Liu <goofysun@gmail.com>
2020-06-12 14:03:17 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -36,7 +36,7 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall
 svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git chinadns-ng
 svn co https://github.com/Lienol/openwrt-package/trunk/package/brook
-
+svn co https://github.com/jsda/packages2/trunk/ntlf9t/luci-app-advancedsetting
 svn co https://github.com/vernesong/OpenClash/branches/master/luci-app-openclash
 
 cd ../../
