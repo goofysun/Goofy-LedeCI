8fc8c79c76da23fd37cac9424ddd61c583a4069c
Jun Liu <goofysun@gmail.com>
2020-06-17 11:38:47 +0800

    修复百度网盘报错问题

--- diy2.sh
+++ diy2.sh
@@ -11,6 +11,8 @@ rm -rf qBittorrent
 rm -rf luci-app-qbittorrent
 rm -rf qt5
 rm -rf rblibtorrent
+rm -rf baidupcs-web
+git clone https://github.com/garypang13/luci-app-baidupcs-web
 git clone https://github.com/lisaac/luci-app-diskman
 git clone https://github.com/brvphoenix/luci-app-wrtbwmon
 git clone https://github.com/brvphoenix/wrtbwmon
