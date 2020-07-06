2c13d1683147e9dfee2d979857d8da97fe028bc9
Jun Liu <goofysun@gmail.com>
2020-07-06 16:40:10 +0800

    Update make_small.sh

--- make_small.sh
+++ make_small.sh
@@ -1,14 +1,13 @@
+sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
+docker container stop $(docker container ls -a -q) && docker system prune --all --force --volumns
+sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php* docker*
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib g++-multilib p7zip p7zip-full msmtp libssl-dev texinfo libreadline-dev libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint ccache curl wget vim nano python python3 python-pip python3-pip python-ply python3-ply haveged lrzsz device-tree-compiler scons antlr3 gperf ecj fastjar mkisofs
+sudo -E apt-get -qq autoremove --purge
+sudo -E apt-get -qq clean
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+sudo df -h
 
-
-# swapoff /swapfile
-# rm -rf /swapfile /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
-# apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
-# apt-get -qq update
-# apt-get -qq install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib g++-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler antlr3 gperf
-# apt-get -qq autoremove --purge
-# apt-get -qq clean
-# df -h
-curl -fsSL https://github.com/goofysun/Goofy-LedeCI/blob/master/.bashrc >> ~/.bashrc
 sudo timedatectl set-timezone "Asia/Shanghai"   
 if [ -d "lede" ];then
   cd lede
