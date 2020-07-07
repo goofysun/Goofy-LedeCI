aac680c201e72a98d641c89f5f8976b73d87066b
Jun Liu <goofysun@gmail.com>
2020-07-07 20:54:10 +0800

    Update make_small.sh

--- make_small.sh
+++ make_small.sh
@@ -25,7 +25,7 @@ fi
 cp -rf ../diy2.sh diy.sh
 chmod +x diy.sh
 ./diy.sh
-cp -rf ../x86_64_small2.config .config
+cp -rf ../x86_64_small.config .config
 make defconfig
 make download -j8 || make download -j1 V=s
 make -j$(nproc) || make -j1 V=s
