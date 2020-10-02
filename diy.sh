d4f8ae481cb2348f79284ebd9dc130fd4f20407b
Jun Liu <goofysun@gmail.com>
2020-10-02 11:27:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,9 +10,10 @@ cd package/custom
 #git https://github.com/KFERMercer/openwrt-baidupcs-web
 #git clone https://github.com/coolsnowwolf/packages
 git clone https://github.com/kenzok8/small
+git clone https://github.com/kenzok8/openwrt-packages
 #修复small库里面V2ray 路径错误问题
 rm -rf ./small/v2ray
-git clone https://github.com/kenzok8/openwrt-packages
+rm -rf ./openwrt-package/package/v2ray
 git clone https://github.com/Lienol/openwrt-package
 #kenzok8 已包含 git clone https://github.com/fw876/helloworld
 #kenzok8 已包含 git clone https://github.com/openwrt-develop/luci-theme-atmaterial
