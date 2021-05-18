3735efb8cd89e666783a1238603a0eed3a76be53
Jun Liu <goofysun@gmail.com>
2021-05-18 16:20:48 +0800

    Update diy-openwrt.sh

--- diy-openwrt.sh
+++ diy-openwrt.sh
@@ -6,35 +6,9 @@ rm -rf package/custom
 mkdir -p package/custom
 #rm -rf package/lean/baidupcs-web
 cd package/custom
-#git https://github.com/KFERMercer/openwrt-baidupcs-web
-#git clone https://github.com/coolsnowwolf/packages
-#git clone https://github.com/kenzok8/small
-#git clone https://github.com/kenzok8/openwrt-packages
-#git clone https://github.com/Lienol/openwrt-package
-
-#修复small库里面V2ray 路径错误问题
-#rm -rf ./small/v2ray
-#rm -rf ./openwrt-package/package/v2ray
-#rm -rf ./openwrt-packages/package/v2ray
-#rm -rf ./openwrt-packages/luci-app-aliddns
-#rm -rf ./openwrt-package/package/v2ray-plugin
-#rm -rf ./openwrt-packages/package/v2ray-plugin
-#rm -rf ./small/v2ray-plugin
-
-#kenzok8 已包含 git clone https://github.com/fw876/helloworld
-#kenzok8 已包含 git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-#kenzok8 已包含 git clone https://github.com/vernesong/OpenClash
-#git clone https://github.com/tty228/luci-app-serverchan
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone https://github.com/jerrykuku/luci-theme-argon
-#git clone https://github.com/lisaac/luci-app-dockerman
-#git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-#git clone https://github.com/honwen/luci-app-aliddns
-#git clone https://github.com/goofysun/luci-app-koolproxyR
-#svn co https://github.com/jsda/packages2/trunk/ntlf9t/luci-app-advancedsetting
-#git clone https://github.com/pymumu/openwrt-smartdns smartdns
-#git clone https://github.com/pymumu/luci-app-smartdns -b lede
+git clone --depth=1 https://github.com/siwind/openwrt-vlmcsd
+git clone --depth=1 https://github.com/siwind/luci-app-vlmcsd
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 cd ../../
 # Modify default IP
@@ -43,7 +17,4 @@ cd ../../
 #sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 # Add some default settings
-
-#\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
-#cp -rf ../zzz-default-settings  package/lean/default-settings/files/zzz-default-settings
 ./scripts/feeds install -a
