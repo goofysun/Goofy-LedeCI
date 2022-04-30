e4d92cfccd4cb84bf531b9eeb30f66b339e2a5a7
Jun Liu <goofysun@gmail.com>
2022-05-01 00:40:21 +0800

    Update diy-slide.sh

--- diy-slide.sh
+++ diy-slide.sh
@@ -25,16 +25,16 @@ cd package/custom
 git clone https://github.com/jerrykuku/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr
 #smartdns
-git clone https://github.com/pymumu/openwrt-smartdns
-git clone https://github.com/pymumu/luci-app-smartdns -b lede
+#git clone https://github.com/pymumu/openwrt-smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns -b lede
 
 #git https://github.com/KFERMercer/openwrt-baidupcs-web
 #git clone https://github.com/coolsnowwolf/packages
 git clone https://github.com/kenzok8/small
-git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/goofysun/openwrt-packages
 rm -rf  ./openwrt-packages/luci-app-aliddns
-rm -rf  ./openwrt-packages/luci-app-smartdns
-rm -rf  ./openwrt-packages/smartdns
+#rm -rf  ./openwrt-packages/luci-app-smartdns
+#rm -rf  ./openwrt-packages/smartdns
 git clone https://github.com/Lienol/openwrt-package
 #修复small库里面V2ray 路径错误问题
 rm -rf ./small/v2ray
