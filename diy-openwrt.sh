8fa062b60f1abbe90cab79576e289f52e156ede0
Jun Liu <goofysun@gmail.com>
2022-05-22 01:26:40 +0800

    Update diy-openwrt.sh

--- diy-openwrt.sh
+++ diy-openwrt.sh
@@ -11,13 +11,36 @@ git clone --depth=1 https://github.com/siwind/luci-app-vlmcsd
 git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/vernesong/OpenClash
+git clone --depth=1 https://github.com/DavBfr/luci-app-openvpn-server
+git clone --depth=1 https://github.com/brvphoenix/wrtbwmon
+git clone --depth=1 https://github.com/brvphoenix/luci-app-wrtbwmon
+
 #helloworld
-git clone https://github.com/jerrykuku/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr
+
+git clone https://github.com/kenzok8/small --depth 1
+git clone https://github.com/kenzok8/openwrt-packages --depth 1
+rm -rf  ./openwrt-packages/luci-app-aliddns 
+git clone https://github.com/Lienol/openwrt-package --depth 1
+git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git --depth 1
+git clone https://github.com/brvphoenix/wrtbwmon --depth 1
+git clone https://github.com/brvphoenix/luci-app-wrtbwmon --depth 1
+#修复small库里面V2ray 路径错误问题
+rm -rf ./small/v2ray
+rm -rf ./openwrt-package/package/v2ray
+rm -rf ./openwrt-packages/package/v2ray
+rm -rf ./openwrt-packages/luci-app-aliddns
+rm -rf ./openwrt-package/package/v2ray-plugin
+rm -rf ./openwrt-packages/package/v2ray-plugin
+rm -rf ./openwrt-packages/luci-app-openclash
+rm -rf ./small/v2ray-plugin
+
+
 cd ../../
 # Modify default IP
 
-#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.100.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 # Add some default settings
