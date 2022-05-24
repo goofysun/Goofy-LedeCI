32f93d249ef97bbbedf51d4eb4a1b19a718a9052
Jun Liu <goofysun@gmail.com>
2022-05-24 22:30:14 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,36 +7,17 @@ rm -rf package/custom
 mkdir -p package/custom
 #rm -rf package/lean/baidupcs-web
 cd package/custom
-#git https://github.com/KFERMercer/openwrt-baidupcs-web
-#git clone https://github.com/coolsnowwolf/packages
 git clone https://github.com/kenzok8/small
 git clone https://github.com/goofysun/openwrt-packages
 git clone https://github.com/Lienol/openwrt-package
 
-#修复small库里面V2ray 路径错误问题
-rm -rf ./small/v2ray
-rm -rf ./openwrt-package/package/v2ray
-rm -rf ./openwrt-packages/package/v2ray
-rm -rf ./openwrt-packages/luci-app-aliddns
-rm -rf ./openwrt-package/package/v2ray-plugin
-rm -rf ./openwrt-packages/package/v2ray-plugin
-rm -rf ./small/v2ray-plugin
 
-#kenzok8 已包含 git clone https://github.com/fw876/helloworld
-#kenzok8 已包含 git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-#kenzok8 已包含 git clone https://github.com/vernesong/OpenClash
 git clone https://github.com/tty228/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
 #git clone https://github.com/jerrykuku/luci-theme-argon -b 18.06
 git clone https://github.com/lisaac/luci-app-dockerman
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/honwen/luci-app-aliddns
 git clone https://github.com/goofysun/luci-app-koolproxyR
 git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git
-#svn co https://github.com/jsda/packages2/trunk/ntlf9t/luci-app-advancedsetting
-#git clone https://github.com/pymumu/openwrt-smartdns smartdns
-#git clone https://github.com/pymumu/luci-app-smartdns -b lede
+
 
 cd ../../
 # Modify default IP
