4e939dcda62d7b9728339212869d42377f600df0
Jun Liu <goofysun@gmail.com>
2022-05-24 12:59:39 +0800

    Update diy-small.sh

--- diy-small.sh
+++ diy-small.sh
@@ -18,36 +18,21 @@ cd package/custom
 
 git clone https://github.com/lisaac/luci-app-dockerman --depth 1
 git clone https://github.com/goofysun/luci-app-koolproxyR --depth 1
-#git clone https://github.com/ElonH/Rclone-OpenWrt && mv -f Rclone-OpenWrt/* ./
-git clone https://github.com/honwen/luci-app-aliddns --depth 1
-#helloworld
-git clone https://github.com/jerrykuku/lua-maxminddb --depth 1
-git clone https://github.com/jerrykuku/luci-app-vssr --depth 1
-#smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns --depth 1
-#git clone https://github.com/pymumu/luci-app-smartdns -b lede --depth 1
 
-#git https://github.com/KFERMercer/openwrt-baidupcs-web --depth 1
-#git clone https://github.com/coolsnowwolf/packages --depth 1
+
 git clone https://github.com/kenzok8/small --depth 1
 git clone https://github.com/kenzok8/openwrt-packages --depth 1
-rm -rf  ./openwrt-packages/luci-app-aliddns 
 git clone https://github.com/Lienol/openwrt-package --depth 1
 git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git --depth 1
 #修复small库里面V2ray 路径错误问题
 rm -rf ./small/v2ray
 rm -rf ./openwrt-package/package/v2ray
 rm -rf ./openwrt-packages/package/v2ray
-rm -rf ./openwrt-packages/luci-app-aliddns
 rm -rf ./openwrt-package/package/v2ray-plugin
 rm -rf ./openwrt-packages/package/v2ray-plugin
 rm -rf ./small/v2ray-plugin
-#kenzok8 已包含 git clone https://github.com/fw876/helloworld --depth 1
-#kenzok8 已包含 git clone https://github.com/openwrt-develop/luci-theme-atmaterial --depth 1
 git clone https://github.com/destan19/OpenAppFilter --depth 1
-#git clone https://github.com/tty228/luci-app-serverchan
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#git clone https://github.com/jerrykuku/luci-theme-argon -b 18.06
+
 #这个有新图标的主题，容易挂
 cd ../../
 
