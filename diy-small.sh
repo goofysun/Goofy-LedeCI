4f4bfadf3a5147066d436fe058b5e17c1f12f9e3
Jun Liu <goofysun@gmail.com>
2022-06-04 02:08:47 +0800

    Update diy-small.sh

--- diy-small.sh
+++ diy-small.sh
@@ -3,8 +3,7 @@
 # 本脚本工作目录必须是git仓库的主目录
 cd package/lean/
 #删除会被替换或者占用安装资源的包
-#rm -rf luci-theme-argon
-#rm -rf luci-app-diskman
+
 rm -rf qBittorrent
 rm -rf luci-app-qbittorrent
 rm -rf qt5
@@ -13,24 +12,12 @@ cd ../../
 
 rm -rf package/custom
 mkdir -p package/custom
-#rm -rf package/lean/baidupcs-web
 cd package/custom
 
-git clone https://github.com/lisaac/luci-app-dockerman --depth 1
-git clone https://github.com/goofysun/luci-app-koolproxyR --depth 1
 
-
-git clone https://github.com/kenzok8/small --depth 1
-git clone https://github.com/kenzok8/openwrt-packages --depth 1
+git clone https://github.com/kenzok8/small-package --depth 1
 git clone https://github.com/Lienol/openwrt-package --depth 1
 git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git --depth 1
-#修复small库里面V2ray 路径错误问题
-#rm -rf ./small/v2ray
-#rm -rf ./openwrt-package/package/v2ray
-#rm -rf ./openwrt-packages/package/v2ray
-#rm -rf ./openwrt-package/package/v2ray-plugin
-#rm -rf ./openwrt-packages/package/v2ray-plugin
-#rm -rf ./small/v2ray-plugin
 git clone https://github.com/destan19/OpenAppFilter --depth 1
 
 #这个有新图标的主题，容易挂
@@ -39,6 +26,3 @@ cd ../../
 # Add some default settings
 cp -rf ../zzz-default-settings  package/lean/default-settings/files/zzz-default-settings
 ./scripts/feeds install -a
-# rm -rf package/feeds/kenzo/luci-app-adguardhome
-# rm -rf package/feeds/kenzo/adguardhome
-# rm -rf package/feeds/kenzo/luci-app-aliddns
