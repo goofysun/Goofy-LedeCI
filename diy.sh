9d1bcb774d494003d59a6aac77ab35dc3d97b56e
Jun Liu <goofysun@gmail.com>
2024-03-16 11:20:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,23 +3,23 @@
 
 # Add Some Package
 #sed -i 's/5.4/4.19/g' ./target/linux/x86/Makefile
-rm -rf package/custom
-mkdir -p package/custom
+#rm -rf package/custom
+#mkdir -p package/custom
 #rm -rf package/lean/baidupcs-web
-cd package/custom
-git clone https://github.com/kenzok8/small
-git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/Lienol/openwrt-package
+#cd package/custom
+#git clone https://github.com/kenzok8/small
+#git clone https://github.com/kenzok8/openwrt-packages
+#git clone https://github.com/Lienol/openwrt-package
 
 
-git clone https://github.com/tty228/luci-app-serverchan
+#git clone https://github.com/tty228/luci-app-serverchan
 #git clone https://github.com/jerrykuku/luci-theme-argon -b 18.06
-git clone https://github.com/lisaac/luci-app-dockerman
-git clone https://github.com/goofysun/luci-app-koolproxyR
-git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git
+#git clone https://github.com/lisaac/luci-app-dockerman
+#git clone https://github.com/goofysun/luci-app-koolproxyR
+#git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git
 
 
-cd ../../
+#cd ../../
 # Modify default IP
 
 #sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
