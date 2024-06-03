120283a8e1313ee4d007d4b8490f3c9eae403785
Jun Liu <goofysun@gmail.com>
2024-06-03 17:15:19 +0800

    Update diy-openwrt.sh

--- diy-openwrt.sh
+++ diy-openwrt.sh
@@ -26,3 +26,4 @@ sed -i 's#../../#$(TOPDIR)/feeds/luci/#g' package/custom/luci-app-openvpn-server
 # Modify default IP
 sed -i 's/192.168.100.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+rm package/feeds/packages/adguardhome -rf
